f8263788c36f7f2be2f892ed8403eae9992e2501
kslotes <nitsugaorom@gmail.com>
2022-10-02 09:06:54 -0300

    Ejercicios anecdotes parte 1

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+
+import { useState } from 'react'
+
+const App = () => {
+   const anecdotes = [
+      'If it hurts, do it more often.',
+      'Adding manpower to a late software project makes it later!',
+      'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      'Premature optimization is the root of all evil.',
+      'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+   ]
+
+   const [selected, setSelected] = useState(0)
+   const [votes, setVotes] = useState([0, 0, 0, 0, 0, 0, 0]);
+   const [mostVotedAnecdote, setMostVotedAnecdote] = useState("");
+
+   const handleGetRandomClick = () => {
+      setSelected(Math.floor(Math.random() * 7));
+   }
+
+
+   const handleVoteClick = () => {
+      const newVotes = [...votes]
+
+      newVotes[selected] += 1
+      setVotes(newVotes)
+   }
+
+   const MostVotedAnecdote = () => {
+      
+      let maxVotes = 0
+      for (let i = 0; i < anecdotes.length; i++) {
+         if (votes[i] > maxVotes) {
+            maxVotes = votes[i]
+            setMostVotedAnecdote(anecdotes[i])
+         }
+
+      }
+      return <p>{mostVotedAnecdote}</p>;
+   }
+
+   return (
+      <div> 
+         <h1>Anecdote of the day</h1>
+         {anecdotes[selected]}
+         <br/>
+         has {votes[selected]} votes.
+         <br/>
+
+         <button onClick={handleVoteClick}>Vote</button>
+         <button onClick={handleGetRandomClick}>Display random story</button>
+         <h1>Anecdote with most votes</h1>
+         <MostVotedAnecdote/>
+      </div>
+   )
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
